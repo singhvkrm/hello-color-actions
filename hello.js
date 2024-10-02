ae3b64372b24cc175a56d833ba77242595362859
singhvkrm <singh.vkrm@gmail.com>
2024-10-01 21:59:08 -0400

    message5

--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
-const hello = () => "Hello Hello World from the Office Hours in the terminal!";
+const hello = () => "Hello World from the Office Hours in the terminal!";
 console.log(hello());
 exports.hello = hello;
 
